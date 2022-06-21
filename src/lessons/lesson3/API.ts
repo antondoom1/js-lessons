85e305e811b07946179d2c8a5ed559a340eb979c
AntonYakavenka <doomanton@gmail.com>
2022-06-21 19:52:33 +0300

    lesson 3

--- src/lessons/lesson3/API.ts
+++ src/lessons/lesson3/API.ts
@@ -1,17 +1,22 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const configOMB = {
-    baseURL: 'http://www.omdbapi.com',
-};
-const key = '';
-const axiosInstance = axios.create(configOMB);
+  baseURL: 'http://www.omdbapi.com'
+}
+const key = '30d0d2fe'
+const axiosInstance = axios.create(configOMB)
 
 const API = {
-    searchFilmsByTitle: (title: string) => {
-    },
-    searchFilmsByType: (title: string, type: string) => {
-    }
-};
+  searchFilmsByTitle: (title: string) => {
+    return axiosInstance
+      .get(`/?apikey=${key}&t=${title}`)
+      .then(response => response.data)
+  },
+  searchFilmsByType: (title: string, type: string) => {
+    return axiosInstance
+      .get(`/?apikey=${key}&s=${title}&type=${type}`)
+      .then(response => response.data)
+  }
+}
 
-
-export default API;
+export default API
--- src/lessons/lesson3/lesson_3.ts
+++ src/lessons/lesson3/lesson_3.ts
@@ -1,4 +1,6 @@
-console.log('lesson 3');
+import axios from 'axios'
+
+console.log('lesson 3')
 
 // Event loop
 // https://learn.javascript.ru/event-loop
@@ -15,6 +17,37 @@ console.log('lesson 3');
 // https://jsonplaceholder.typicode.com/posts/1
 // https://habr.com/ru/company/oleg-bunin/blog/417461/?_ga=2.54695343.543933152.1602500664-1040035071.1596811661
 
+axios
+  .get('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => console.log(response))
 
-// just a plug
-export default ()=>{};
+axios
+  .post('https://jsonplaceholder.typicode.com/posts', {
+    body: {
+      title: 'foo',
+      body: 'bar',
+      userId: 1
+    },
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  })
+  .then(response => console.log(response))
+
+axios
+  .put('https://jsonplaceholder.typicode.com/posts/1', {
+    body: {
+      id: 1,
+      title: 'foo',
+      body: 'bar',
+      userId: 1
+    },
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  })
+  .then(response => console.log(response))
+
+axios
+  .delete('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => console.log(response))
